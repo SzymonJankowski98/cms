15d7669656117048673cc9f99078c29fe5312fc2
SzymonJankowski98 <jankowski.szymon.98@gmail.com>
2022-12-25 12:29:10 +0100

    Init project

---
+++ src/lib/components/Toolbar.svelte
@@ -0,0 +1,28 @@
+<script>
+  import Fa from 'svelte-fa'
+  import { faSquarePlus, faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+
+  export let type = "horizontal";
+</script>
+
+<div class="group relative">
+  <slot></slot>
+  <div class="hidden absolute {type} top-0 group-hover:flex bg-slate-400">
+    <button class="p-1 flex justify-center items-center">
+      <Fa class="text-lg text-white" icon={faSquarePlus}/>
+    </button>
+    <button class="p-1">
+      <Fa class="text-lg text-white" icon={faPenToSquare}/>
+    </button>
+  </div>
+</div>
+
+<style>
+  .horizontal {
+    @apply left-0;
+  }
+
+  .vertical {
+    @apply right-0 flex-col;
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+  import Toolbar from "$lib/components/Toolbar.svelte";
+
+  let page = [
+    [
+      { content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi ducimus ipsam modi porro earum nobis? Cupiditate, ducimus dignissimos explicabo magni quod eius nemo, iure consequatur quasi quaerat sequi perspiciatis. Quos." },
+      { content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi ducimus ipsam modi porro earum nobis? Cupiditate, ducimus dignissimos explicabo magni quod eius nemo, iure consequatur quasi quaerat sequi perspiciatis. Quos." }
+    ],
+    [
+      { content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi ducimus ipsam modi porro earum nobis? Cupiditate, ducimus dignissimos explicabo magni quod eius nemo, iure consequatur quasi quaerat sequi perspiciatis. Quos." },
+      { content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi ducimus ipsam modi porro earum nobis? Cupiditate, ducimus dignissimos explicabo magni quod eius nemo, iure consequatur quasi quaerat sequi perspiciatis. Quos." }
+    ],
+    [
+      { content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi ducimus ipsam modi porro earum nobis? Cupiditate, ducimus dignissimos explicabo magni quod eius nemo, iure consequatur quasi quaerat sequi perspiciatis. Quos." },
+      { content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi ducimus ipsam modi porro earum nobis? Cupiditate, ducimus dignissimos explicabo magni quod eius nemo, iure consequatur quasi quaerat sequi perspiciatis. Quos." }
+    ]
+  ];
+</script>
+
+<head>
+    <title>Edit Page</title>
+</head>
+
+<main  class="flex flex-col">
+  {#each page as row}
+    <Toolbar type="vertical">
+      <div class="flex flex-row justify-between">
+        {#each row as { content }}
+          <Toolbar>
+            <div class="px-8 py-2 flex-grow border border-black">
+              {content}
+            </div>
+          </Toolbar>
+        {/each}
+      </div>
+    </Toolbar>
+  {/each}
+</main>
